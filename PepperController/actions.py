147e67ea79636089cddb74bac0906aba8e7a023e
ScorIsAFox <rixingyingyao@outlook.com>
2023-08-31 15:22:49 +1200

    Add reply for execute not pre-coded actions

--- PepperController/actions.py
+++ PepperController/actions.py
@@ -90,6 +90,12 @@ class Actions:
         self.actions_dict["sing"] = self.sing
         self.actions_dict["rotate"] = self.rotate
 
+    def notExistReply(self):
+        action = self.action_think[random.randint(0, len(self.action_think) - 1)]
+        content = "I am sorry but my developer did not design me to do this physical action."
+        command = "^start(%s) %s...^wait(%s)" % (action, content, action)
+        self.tts.say(command)
+
 
 if __name__ == "__main__":
     a = Actions("172.22.1.21", 9559)
